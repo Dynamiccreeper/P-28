3b07a60a07ec7a738ddc8ee08191e091320dc460
Dynamiccreeper <74767888+Dynamiccreeper@users.noreply.github.com>
2021-03-31 10:45:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
 const Constraint=Matter.Constraint;
+
 var treeObj, stoneObj,groundObject, launcherObject;
 var mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8,mango9,mango10,mango11,mango12;
 var world,boy;
@@ -37,15 +38,7 @@ function setup() {
 	treeObj=new tree(1050,580);
 	groundObject=new ground(width/2,600,width,20);
 	launcherObject=new launcher(stoneObj.body,{x:235,y:420})
-  // var render = Render.create({
-  //   element: document.body,
-  //   engine: engine,
-  //   options: {
-  //     width: 1300,
-  //     height: 600,
-  //     wireframes: false
-  //   }
-  // });
+
 	
 	Engine.run(engine);
  // Render.run(render);
@@ -54,12 +47,11 @@ function setup() {
 function draw() {
 
   background(230);
-  //frameRate(2)
- // Engine.update(engine)
+
   textSize(25);
   text("Press Space to get a second Chance to Play!!",50 ,50);
   image(boy ,200,340,200,300);
-  //Engine.update(engine)
+
   
 
   treeObj.display();
@@ -101,7 +93,7 @@ function mouseDragged()
 function mouseReleased()
 {
 	launcherObject.fly();
-    // distance=int(dist(stoneObj.x,stoneObj.y,mango1.x,mango1.y));
+
 }
 
 function keyPressed() {
@@ -112,20 +104,15 @@ function keyPressed() {
   }
 
   function detectollision(lstone,lmango){
-	/*var collision = Matter.SAT.collides(lstone,lmango);
-	if(collision.collided){
-		console.log("collided");
-		Matter.Body.setStatic(lmango,false);	
-	}*/
+
   mangoBodyPosition=lmango.body.position
   stoneBodyPosition=lstone.body.position
   
   var distance=dist(stoneBodyPosition.x, stoneBodyPosition.y, mangoBodyPosition.x, mangoBodyPosition.y)
-  //console.log(distance)
- // console.log(lmango.r+lstone.r)
+
   	if(distance<=lmango.r+lstone.r)
     {
-      //console.log(distance);
+
   	  Matter.Body.setStatic(lmango.body,false);
     }
 
